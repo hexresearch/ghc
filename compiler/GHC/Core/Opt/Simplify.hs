89208c0f83b7737471eaa815c6d815e5488de2bc
Simon Peyton Jones <simon.peytonjones@gmail.com>
2022-01-04 15:10:45 +0000

    Final fixups to eta/arity changes
    Just comments and minor refactoring

--- compiler/GHC/Core/Opt/Simplify.hs
+++ compiler/GHC/Core/Opt/Simplify.hs
@@ -386,7 +386,7 @@ simplLazyBind env top_lvl is_rec bndr bndr1 rhs rhs_se
                         ; return (poly_floats, body3) }
 
         ; let env' = env `setInScopeFromF` rhs_floats
-        ; rhs' <- rebuildLam env' tvs' body3 rhs_cont
+        ; rhs' <- rebuildLam env' tvs' body3 (Just is_rec)
         ; (bind_float, env2) <- completeBind env' top_lvl is_rec Nothing bndr bndr1 rhs'
         ; return (rhs_floats `addFloats` bind_float, env2) }
 
@@ -964,7 +964,7 @@ addLetBndrInfo new_bndr new_arity_type new_unf
 
     -- Bottoming bindings: see Note [Bottoming bindings]
     info4 | isDeadEndDiv div = info3 `setDmdSigInfo` bot_sig
-                                     `setCprSigInfo`        bot_cpr
+                                     `setCprSigInfo` bot_cpr
           | otherwise        = info3
 
     bot_sig = mkClosedDmdSig (replicate new_arity topDmd) div
@@ -982,12 +982,12 @@ Suppose we have
    let x = error "urk"
    in ...(case x of <alts>)...
 or
-   let f = \x. error (x ++ "urk")
+   let f = \y. error (y ++ "urk")
    in ...(case f "foo" of <alts>)...
 
 Then we'd like to drop the dead <alts> immediately.  So it's good to
-propagate the info that x's RHS is bottom to x's IdInfo as rapidly as
-possible.
+propagate the info that x's (or f's) RHS is bottom to x's (or f's)
+IdInfo as rapidly as possible.
 
 We use tryEtaExpandRhs on every binding, and it turns out that the
 arity computation it performs (via GHC.Core.Opt.Arity.findRhsArity) already
@@ -1647,7 +1647,7 @@ simpl_lam env bndr body cont
   = do  { let (inner_bndrs, inner_body) = collectBinders body
         ; (env', bndrs') <- simplLamBndrs env (bndr:inner_bndrs)
         ; body'   <- simplExpr env' inner_body
-        ; new_lam <- rebuildLam env' bndrs' body' cont
+        ; new_lam <- rebuildLam env' bndrs' body' (contIsRhs cont)
         ; rebuild env' new_lam cont }
 
 -------------
